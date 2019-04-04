fd44107c4b525d00aea52638fd5d34e972cd1bbf
Arnaud Loonstra <arnaud@sphaero.org>
2019-04-04 10:10:21 +0200

    added includes

---
+++ include/libsphactor.h
@@ -0,0 +1,23 @@
+/*  =========================================================================
+    libsphactor - Extended nodal actor framework based on zactor
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.
+
+    This file is part of Zyre, an open-source framework for proximity-based
+    peer-to-peer applications -- See http://zyre.org.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    =========================================================================
+*/
+
+#ifndef LIBSPHACTOR_H_H_INCLUDED
+#define LIBSPHACTOR_H_H_INCLUDED
+
+//  Include the project library file
+#include "sphactor_library.h"
+
+//  Add your own public definitions here, if you need them
+
+#endif
