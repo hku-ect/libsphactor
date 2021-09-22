6c04f2a660b5c653c0f7c33c2c92810ddf63adeb
Arnaud Loonstra <arnaud@sphaero.org>
2021-09-22 18:36:36 +0200

    initial code for running an actor as a process

--- src/sph.c
+++ src/sph.c
@@ -68,14 +68,43 @@ print_help()
     //exit(EXIT_SUCCESS);
 }*/
 
+static void
+s_self_switch (zsock_t *input, zsock_t *output)
+{
+    //  We use the low-level libzmq API for best performance
+    void *zmq_input = zsock_resolve (input);
+    void *zmq_output = zsock_resolve (output);
+    //void *zmq_capture = self->capture? zsock_resolve (self->capture): NULL;
+
+    zmq_msg_t msg;
+    zmq_msg_init (&msg);
+    while (true) {
+        if (zmq_recvmsg (zmq_input, &msg, ZMQ_DONTWAIT) == -1)
+            break;      //  Presumably EAGAIN
+        int send_flags = zsock_rcvmore (input)? ZMQ_SNDMORE: 0;
+        /*if (zmq_capture) {
+            zmq_msg_t dup;
+            zmq_msg_init (&dup);
+            zmq_msg_copy (&dup, &msg);
+            if (zmq_sendmsg (zmq_capture, &dup, send_flags) == -1)
+                zmq_msg_close (&dup);
+        }*/
+        if (zmq_sendmsg (zmq_output, &msg, send_flags) == -1) {
+            zmq_msg_close (&msg);
+            break;
+        }
+    }
+}
+
+
 int main (int argc, char *argv [])
 {
     bool verbose = false;
     zargs_t *args = zargs_new(argc, argv);
     assert(args);
     zsys_init();
-    if ( zargs_arguments(args) == 0 )
-        return print_help();
+    //if ( zargs_arguments(args) == 0 )
+    //    return print_help();
 
     if ( zargs_hasx (args, "--help", "-h", NULL) )
         return print_help();
@@ -83,27 +112,70 @@ int main (int argc, char *argv [])
     if (zargs_hasx(args, "--verbose", "-v", NULL) )
         verbose = true;
 
-    //  Insert main code here
-    if (verbose)
-        zsys_info ("sph - ");
-
     //register_dyn_libs("bla");
     // register stock actors
     sphactor_register( "Log", &sph_stock_log_actor, NULL, NULL );
     sphactor_register( "Count", &sph_stock_count_actor, NULL, NULL );
     sphactor_register( "Pulse", &sph_stock_pulse_actor, NULL, NULL );
 
-    const char *conffile = zargs_first(args);
-
-    sph_stage_t *stage = sph_stage_new("clistage");
-    sph_stage_load(stage, conffile);
-
-    while (!zsys_interrupted)
+    int rc = 0;
+    const char *act = zargs_get(args, "--actor");
+    if ( act )
     {
-        zclock_sleep(300);
+        // run single actor with proxy
+
+        // try to create an actor from given actor
+        assert(act);
+        sphactor_t *actor = sphactor_new_by_type(act, "Process Actor", NULL);
+        assert(actor);
+        zsock_t *act_sock = sphactor_socket(actor);
+
+        zsock_t *ctrlsock = zsock_new_pull("tcp://*:*");
+        assert(ctrlsock);
+        const char *ctrlendp = zsock_endpoint(ctrlsock);
+
+        zpoller_t *poller = zpoller_new (act_sock, ctrlsock, NULL);
+        zpoller_set_nonstop(poller, true);
+
+        while (!zsys_is_interrupted())
+        {
+            zsock_t *ready = zpoller_wait(poller, 100 * ZMQ_POLL_MSEC);
+            if (ready == NULL) continue; // timeout
+            else if  (ready == act_sock)
+            {
+                s_self_switch(act_sock, ctrlsock);
+            }
+            else
+            {
+                assert (ready == ctrlsock);
+                s_self_switch(ctrlsock, act_sock);
+            }
+        }
+        zpoller_destroy(&poller);
+        sphactor_destroy(&actor);
+        zsock_destroy(&ctrlsock);
     }
-    sph_stage_clear(stage);
-    zsys_info("EXIT");
-    zargs_destroy(&args);
-    return 0;
+    else
+    {
+
+        //  Insert main code here
+        if (verbose)
+            zsys_info ("sph - ");
+
+        const char *conffile = zargs_first(args);
+
+        sph_stage_t *stage = sph_stage_new("clistage");
+        sph_stage_load(stage, conffile);
+
+        while (!zsys_interrupted)
+        {
+            zclock_sleep(300);
+        }
+        sph_stage_clear(stage);
+        zsys_info("EXIT");
+        zargs_destroy(&args);
+    }
+
+    sphactor_dispose();
+    return rc;
 }
